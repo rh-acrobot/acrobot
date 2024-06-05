960d23731a8828bf1fc9bfcab6c2de9cfd1e3e36
Janet Cobb <jacobb@redhat.com>
2024-06-05 10:19:35 -0400

    Add Slack dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("com.slack.api:bolt-socket-mode:1.39.3")
+    implementation("javax.websocket:javax.websocket-api:1.1")
+    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:1.19")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
