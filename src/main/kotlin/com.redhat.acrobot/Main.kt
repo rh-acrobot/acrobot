992e38ab2208d1757bfec9b192416178d7f2b417
Janet Cobb <jacobb@redhat.com>
2024-06-05 11:11:25 -0400

    Add basic event handling

--- src/main/kotlin/com.redhat.acrobot/Main.kt
+++ src/main/kotlin/com.redhat.acrobot/Main.kt
@@ -2,12 +2,29 @@ package com.redhat.acrobot
 
 import com.slack.api.bolt.App
 import com.slack.api.bolt.socket_mode.SocketModeApp
+import com.slack.api.model.event.AppMentionEvent
 
 fun main() {
     val app = App()
 
     app.command("/hello") { req, ctx ->
         ctx.ack(":wave: Hello!")
+    }
+
+    app.event(AppMentionEvent::class.java) { payload, ctx ->
+        ctx.logger.info("Got mention event")
+
+        val response = ctx.client().chatPostMessage {
+            it.channel(payload.event.channel)
+            it.threadTs(payload.event.threadTs)
+            it.text("I have been summoned!")
+        }
+
+        if (!response.isOk) {
+            ctx.logger.error("Failed to respond: ${response.error}")
+        }
+
+        ctx.ack()
     }
 
     SocketModeApp(app).start()
