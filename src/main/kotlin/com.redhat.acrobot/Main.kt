75cf1a584ba220d63c0cf76b6a74a49dea3b808c
Janet Cobb <jacobb@redhat.com>
2024-06-05 11:45:46 -0400

    Add basic message event handler

--- src/main/kotlin/com.redhat.acrobot/Main.kt
+++ src/main/kotlin/com.redhat.acrobot/Main.kt
@@ -3,6 +3,7 @@ package com.redhat.acrobot
 import com.slack.api.bolt.App
 import com.slack.api.bolt.socket_mode.SocketModeApp
 import com.slack.api.model.event.AppMentionEvent
+import com.slack.api.model.event.MessageEvent
 
 fun main() {
     val app = App()
@@ -18,6 +19,22 @@ fun main() {
             it.channel(payload.event.channel)
             it.threadTs(payload.event.threadTs)
             it.text("I have been summoned!")
+        }
+
+        if (!response.isOk) {
+            ctx.logger.error("Failed to respond: ${response.error}")
+        }
+
+        ctx.ack()
+    }
+
+    app.event(MessageEvent::class.java) { payload, ctx ->
+        ctx.logger.info("Channel type: " + payload.event.channelType)
+
+        val response = ctx.client().chatPostMessage {
+            it.channel(payload.event.channel)
+            it.threadTs(payload.event.threadTs)
+            it.text("ACK! You surprised me")
         }
 
         if (!response.isOk) {
