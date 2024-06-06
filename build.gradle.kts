ade465bbb9851e3b2c330829878fb8fa7f40990d
Janet Cobb <jacobb@redhat.com>
2024-06-06 13:55:33 -0400

    Add hibernate-jpamodelgen

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,10 @@ dependencies {
     implementation("javax.websocket:javax.websocket-api:1.1")
     implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:1.19")
     implementation("org.slf4j:slf4j-simple:1.7.36")
-    implementation("org.hibernate.orm:hibernate-core:6.5.2.Final")
     implementation("com.mysql:mysql-connector-j:8.4.0")
+
+    implementation("org.hibernate.orm:hibernate-core:6.5.2.Final")
+    annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:6.5.2.Final")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
