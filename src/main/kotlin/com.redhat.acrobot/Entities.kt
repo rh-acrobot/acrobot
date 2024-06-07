95afb8063685c6707b2a41b45c3cb6734c04fd0c
Janet Cobb <jacobb@redhat.com>
2024-06-07 08:38:58 -0400

    Actually create acronyms if they do not exist

--- src/main/kotlin/com.redhat.acrobot/Entities.kt
+++ src/main/kotlin/com.redhat.acrobot/Entities.kt
@@ -10,6 +10,7 @@ fun findOrCreateAcronym(session: Session, text: String): Acronym {
     val normalizedText = Acronym.normalizeText(text)
 
     return session.byNaturalId(Acronym::class.java).using(Acronym_.acronym, normalizedText).load()
+        ?: Acronym(normalizedText).also { session.persist(it) }
 }
 
 fun findExplanation(session: Session, acronym: Acronym, text: String): Explanation? {
