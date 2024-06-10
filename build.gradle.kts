62892be8b0180734246fc59c8ad7923ec2639ab5
Janet Cobb <jacobb@redhat.com>
2024-06-10 08:22:14 -0400

    Add explicit JUnit dependencies to Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testRuntimeOnly("com.h2database:h2:2.2.224")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 tasks.test {
